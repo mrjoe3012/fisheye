1fa54e7cc05a293cda7059818961d1fd93505f7c
Joseph Agrane <josephagrane@gmail.com>
2025-04-05 08:22:53 +0200

    added empty python package and gitignore

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='fisheye',
+    version='0.0.1',
+    description='A library for calibrating and working with fisheye cameras.',
+    license='TOOD',
+    maintainer='Joseph Agrane',
+    maintainer_email='josephagrane@gmail.com',
+    author='Joseph Agrane',
+    author_email='josephagrane@gmail.com',
+    install_requires=[
+
+    ]
+)
